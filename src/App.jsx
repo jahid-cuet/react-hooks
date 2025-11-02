1c85f5af32b204d6976c5fa909073a24b16b467c
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2025-11-02 20:15:33 +0600

    useEffect-hook-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
+import { useState } from 'react';
+import MyComponent from './useEffect/MyComponent';
 
-import './App.css'
-import TodoClass from './Todo/TodoClass'
-import Todo from './Todo/Todo'
-import Counter from './Counter'
+export default function App() {
+    const [show, setShow] = useState(true);
 
-function App() {
-
-
-  return (
-    <>
-    <Todo/>
-    <Counter/>
-    </>
-    
-  )
+    return (
+        <div className="app">
+            <div>{show && <MyComponent />}</div>
+            <p>
+                <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
+                    {show ? 'Hide post' : 'Show post'}
+                </button>
+            </p>
+        </div>
+    );
 }
-
-export default App
